2f6ecc5637dd378837c503cb0cc48dc90585f70e
tobykentrunzhi <396367738@qq.com>
2015-10-25 22:30:07 +0800

    project

--- Crazy1/ViewController.h
+++ Crazy1/ViewController.h
@@ -8,8 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
--- Crazy1/ViewController.m
+++ Crazy1/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIBarButtonItem *done;
+    UINavigationItem *navItem;
+    
+}
 
 @end
 
@@ -17,11 +22,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.textView.delegate=self;
+    UINavigationBar * navBar= [[UINavigationBar alloc]initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 44)];
+    [self.view addSubview:navBar];
+    
+    
+    navItem=[[UINavigationItem alloc]initWithTitle:@"导航条"];
+    navBar.items=@[navItem]; //这个是什么语法
+    
+    
+    done=[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(finishEdit)];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+    navItem.rightBarButtonItem=done;
+}
+-(void)textViewDidEndEditing:(UITextView *)textView{
+    navItem.rightBarButtonItem=nil;
+    
+}
+
+-(void)finishEdit{
+    [self.textView resignFirstResponder];
+    
+}
+
 
 @end
